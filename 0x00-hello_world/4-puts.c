f05d5cb5e3914fd0f46c96045629fbd9c31435fe
jodt <joel.dumortier@gmail.com>
2021-09-30 11:06:33 +0000

    modified script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
-* display text on screen
+* main - Display string with puts function
+* Return: return 0
 */
 int main(void)
 {
-        char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
         puts(str);
         return (0);
