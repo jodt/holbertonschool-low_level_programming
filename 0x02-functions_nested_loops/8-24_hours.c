a20880c5999aa6a26f2b8bbf032f4af3a801f147
jodt <joel.dumortier@gmail.com>
2021-10-05 12:21:55 +0000

    modified script jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * jack_bauer - prints every minute  starting from 00:00 to 23:59.
+ * jack_bauer - prints every minute  starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
