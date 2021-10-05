817503c30a62f2b24612f25de5b5a186ad25c0d0
jodt <joel.dumortier@gmail.com>
2021-10-05 19:15:07 +0000

    modified project natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 /**
  * natural - @computes and prints sum of all the multiples of 3 or 5 below 1024
  *
@@ -8,7 +9,7 @@ int main(void)
 	int nb = 1024, i;
 	int result = 0;
 
-	for (i = 0, i <= nb; i++)
+	for (i = 0; i <= nb; i++)
 	{
 		if (i % 3 == 0)
 			result += i;
