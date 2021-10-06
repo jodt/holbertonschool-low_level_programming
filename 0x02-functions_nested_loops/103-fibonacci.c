20ec2ee758cce32127badd8c5f3bf8393dea80a4
jodt <joel.dumortier@gmail.com>
2021-10-06 19:17:54 +0000

    project fibonacci 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - finds and prints the sum of the even-valued the Fibonacci sequence
+ * Return: return 0
+ */
+int main(void)
+{
+	long int x = 0, y = 1, i, somme, evensum;
+
+	for (i = 0; i < 100; i++)
+	{
+		somme = x + y;
+		if (somme > 4000000)
+			break;
+		if (somme % 2 == 0)
+			evensum += somme;
+		x = y;
+		y = somme;
+	}
+	printf("%ld\n", evensum);
+	return (0);
+}
