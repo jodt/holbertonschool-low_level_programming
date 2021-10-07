f05e3393c27e1e9fdd686a90b5a1eca497280523
jodt <joel.dumortier@gmail.com>
2021-10-07 14:23:51 +0000

    modified project 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * print_number - print an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	int count = 0, temp, diviseur = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	temp = n;
+	while (temp > 9)
+	{
+		temp = temp / 10;
+		count++;
+		diviseur *= 10;
+	}
+	while (count >= 0)
+	{
+		temp = n / diviseur;
+		_putchar('0' + temp);
+		n -= (temp * diviseur);
+		diviseur /= 10;
+		count--;
+	}
+}
