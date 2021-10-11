0767e5d2b877d6fdcfb54edfccd22fdd008b7bde
jodt <joel.dumortier@gmail.com>
2021-10-11 16:29:28 +0000

    project 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include"main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ *
+ *Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, exponent = 0, number = 0, position_first_integer = 0;
+	
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 45){
+			position_first_integer = i;
+			break;
+		}
+	}
+	i = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 47 && s[i] < 58)
+			exponent++;
+	}         
+        i = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 47 && s[i] < 58)
+		{
+			while (exponent > 1)
+			{
+				number += (s[i] - 48) * 10;
+				exponent--;
+			}
+			i++;
+			number += (s[i] - 48);
+			if (s[position_first_integer - 1] == 45)
+				number = -number;
+		}
+		else
+		{
+			return(0);
+		}
+	}
+	return (number);
+}
