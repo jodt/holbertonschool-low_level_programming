1bf3f3ae10785b4db8894d45e9ae7e6cf26a663f
jodt <joel.dumortier@gmail.com>
2021-10-12 07:49:07 +0000

    project 10 modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,38 @@
 #include"main.h"
+#include<stdio.h>
 /**
  * _atoi - convert a string to an integer
  * @s: string
  *
- *Return: integer
+ *Return: integer, 0 if not integer
  */
 int _atoi(char *s)
 {
-	int i, exponent = 0, number = 0, position_first_integer = 0;
-	
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] > 45){
-			position_first_integer = i;
-			break;
-		}
-	}
-	i = 0;
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] > 47 && s[i] < 58)
-			exponent++;
-	}         
-        i = 0;
+	int i, is_integer = 0, nombre = 0, sign = 1, first_digit = 0;
+
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] > 47 && s[i] < 58)
 		{
-			while (exponent > 1)
-			{
-				number += (s[i] - 48) * 10;
-				exponent--;
-				i++;
-			}
-			number += (s[i] - 48);
-			if (s[position_first_integer - 1] == 45)
-			{
-			number = -number;
-			}
+			first_digit = i;
+			is_integer++;
+			break;
 		}
 	}
-	return (number);
+	if (is_integer == 0)
+		return (0);
+	i = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 47 && s[i] < 58)
+		{
+			nombre = nombre * 10 + (s[i] - '0');
+		if ((s[i + 1] < 47) || (s[i] + 1 > 58))
+			break;
+		}
+	}
+	if (s[first_digit - 1] == 45)
+		sign = -1;
+	nombre = nombre * sign;
+	return (nombre);
 }
