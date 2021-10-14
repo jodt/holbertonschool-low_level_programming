ea8157b7bfb2cd1a266008c9a7cdbc85591820cb
jodt <joel.dumortier@gmail.com>
2021-10-14 09:16:21 +0000

    add project 101 print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_number - prints an integer
+ * @n : integer
+ */
+void print_number(int n)
+{
+	int temp = 0;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	temp = n % 10;
+	n = (n - temp);
+	if (n)
+	{
+		print_number(n / 10);
+	}
+	_putchar(temp + '0');
+}
