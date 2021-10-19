1e618295c5b4fd2175be415ce6ef249abc189318
jodt <joel.dumortier@gmail.com>
2021-10-19 12:37:21 +0000

    bug fix

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,85 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: the length;
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+/**
+ * rev_string - reverse array of string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i = 0, end = 0, j, temp;
+
+	while (s[end])
+		end++;
+	j = end - 1;
+	while (i < j)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		i++;
+		j--;
+	}
+}
+/**
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer
+ * @size_r: size buffer
+ *
+ * Return: result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, addition = 0;
+
+	i = _strlen(n1) - 1;
+	j = _strlen(n2) - 1;
+	for (k = 0; i >= 0 || j >= 0; k++)
+	{
+		if (i >= 0 && j >= 0)
+			addition += (n1[i] - '0') + (n2[j] - '0');
+		else if (i < 0)
+			addition += n2[j] - '0';
+		else if (j < 0)
+			addition += n1[i] - '0';
+		if (addition > 9)
+		{
+			r[k] = (addition % 10) + '0';
+			addition = 1;
+		}
+		else
+		{
+			r[k] = addition + '0';
+			addition = 0;
+		}
+		if (i >= 0)
+			i--;
+		if (j >= 0)
+			j--;
+	}
+	if (i < 0 && j < 0 && addition == 1)
+	{
+		r[k] = 1 + '0';
+		r[k + 1] = '\0';
+		k++;
+	}
+	rev_string(r);
+	r[k] = '\0';
+	if (k >= size_r)
+		return (0);
+	return (r);
+}
