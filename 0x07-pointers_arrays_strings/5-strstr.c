07b53e8fea2408a521cbb4fdd722a4af65614300
jodt <joel.dumortier@gmail.com>
2021-10-19 09:46:33 +0000

    code clean

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,37 +1,57 @@
 #include"main.h"
 #define NULL 0
 /**
- * _strstr - finds the first occurrence of a substring
- * @needle: substring
+ * find_first_char - find the first character which match
  * @haystack: string
+ * @needle: second string
  *
- * Return: pointer to the beginning of the located substring, or NULL
+ * Return: Position of character in the first string
+ */
+int find_first_char (char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (haystack[i] != needle[0])
+	{
+		if ((!(haystack[i])) || (!(needle[i])))
+			return (0);
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strstr -  locates a substring
+ * @haystack: string
+ * @needle: word
+ *
+ * Return: pointer to the beginning of the located substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0, length_needle = 0, count = 0, temp = 0;
+	int i = 0, position, j = 0;
 
-	while (needle[i])
+	position = find_first_char(haystack, needle);
+	while  (haystack[i] && needle[j])
 	{
-		length_needle++;
-		i++;
-	}
-	i = 0;
-	while (haystack[i])
-	{
-		temp = i;
-		while (needle[j])
+		if (haystack[i] == needle[j])
 		{
-			if (haystack[i] == needle[j])
-				count++;
 			i++;
 			j++;
 		}
-		if (count == length_needle)
-			return (haystack + temp);
-		j = count = 0;
-		i = temp;
-		i++;
+		else
+		{
+			while (haystack[i] != needle[0])
+			{
+				if (!(haystack[i]))
+					return (NULL);
+				i++;
+			}
+			j = 0;
+			position = i;
+		}
 	}
-	return (NULL);
+	if (!(needle[j]))
+		return (haystack + position);
+	else
+		return (NULL);
 }
