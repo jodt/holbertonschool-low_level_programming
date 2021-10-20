912171a2297d5e42d7dcb721751242898e00b675
jodt <joel.dumortier@gmail.com>
2021-10-20 19:24:43 +0000

    completed project 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include"main.h"
+/**
+ * lengthofstring - find length of string
+ * @n: integer
+ * @s: string
+ *
+ * Return: length
+ */
+int lengthofstring(int n, char *s)
+{
+	if (s[n] == '\0')
+		return (n);
+	return (lengthofstring(n + 1, s));
+}
+
+/**
+ * issamecharacter - check character by character
+ * @s: string
+ * @start: value of depart
+ * @length: length of the string
+ * Return: 1 or 0
+ */
+int issamecharacter(char *s, int  start, int length)
+{
+	int i = 0;
+
+	if (s[start] != s[length] && i <= length / 2)
+		return (0);
+	if (s[start] == s[length] && i == length / 2)
+		return (1);
+	i++;
+	return (issamecharacter(s, start + 1, length - 1));
+}
+/**
+ * is_palindrome - check  if a string is a palindrome
+ * @s: string
+ *
+ * Return: 1 if string is a palindrome
+ */
+int is_palindrome(char *s)
+{
+	int start = 0, length;
+
+	length = lengthofstring(0, s);
+	length -= 1;
+	return (issamecharacter(s, start, length));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,4 @@ Project 3 : function that returns the factorial of a given number
 Project 4 : function that returns the value of x raised to the power of y
 Project 5 : function that returns the natural square root of a number
 Project 6 : function that returns 1 if the input integer is a prime number, otherwise return 0
+Project 7 : function that returns 1 if a string is a palindrome and 0 if not
