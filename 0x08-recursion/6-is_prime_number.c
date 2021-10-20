d1f0af39f688aa4b2edd30d2a4e0299fd17644bd
jodt <joel.dumortier@gmail.com>
2021-10-20 15:15:30 +0000

    Project 6 completed

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include"main.h"
+/**
+ * checkprime - check if a integer is a prime number
+ * @n: integer
+ * @i: index
+ *
+ * Return: 1 if is integer
+ */
+int checkprime(int n, int i)
+{
+	if (n % i == 0 && i != n)
+		return (0);
+	if (i == n)
+		return (1);
+	return (checkprime(n, i + 1));
+}
+
+/**
+ * is_prime_number - find  if the input integer is a prime number
+ * @n integer
+ *
+ * Return: 1 if the integer is a prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (checkprime(n, 2));
+}
+
+
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -6,3 +6,4 @@ Project 2 : function that returns the length of a string
 Project 3 : function that returns the factorial of a given number
 Project 4 : function that returns the value of x raised to the power of y
 Project 5 : function that returns the natural square root of a number
+Project 6 : function that returns 1 if the input integer is a prime number, otherwise return 0
