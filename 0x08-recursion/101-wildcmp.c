5cc447cce15b31283fd78acc8f4052e484b6b12c
jodt <joel.dumortier@gmail.com>
2021-10-21 19:23:38 +0000

    betty bug fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,8 +16,8 @@ int checkcharacter(char *s1, char *s2)
 		return (checkcharacter(s1 + 1, s2));
 	if (*s1 == *s2 && *(s2 + 1) && *(s2 + 1) != '*')
 		return (checkcharacter(s1 + 1, s2 + 1));
-	if (*s1 != *s2 && *s1 && *(s2+1) !='*')
-                return (checkcharacter(s1 + 1, s2));
+	if (*s1 != *s2 && *s1 && *(s2 - 1) != '*')
+		return (checkcharacter(s1 + 1, s2));
 	if (*s1 == *s2 && !*(s1 + 1) && !*(s2 + 1))
 		return (1);
 	if (*s1 != *s2)
