db9433557e1f3aca8e86e866a684837ce338e1c3
jodt <joel.dumortier@gmail.com>
2021-10-26 09:23:55 +0000

    project 2 completed

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include<stdlib.h>
+#include"main.h"
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ *Return: pointer to concatenated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int lengths1, lengths2, i, j, k;
+
+	if (s1 == NULL)
+		lengths1 = 1;
+	else
+		lengths1 = _strlen(s1);
+	if (s2 == NULL)
+		lengths2 = 1;
+	else
+		lengths2 = _strlen(s2);
+	ptr = malloc((lengths1 + lengths2 + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0, j = 0; j < lengths1; i++, j++)
+		ptr[i] = s1[j];
+	for (k = 0; k < lengths2; i++, k++)
+		ptr[i] = s2[k];
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: the length;
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -2,3 +2,5 @@
 
 Project 0 : function that creates an array of chars, and initializes it with a specific char
 Project 1 : function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
+Project 2 : function that concatenates two strings
+
