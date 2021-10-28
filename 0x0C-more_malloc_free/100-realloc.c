dce898707daab9543efa0237db81913c0cf6d852
jodt <joel.dumortier@gmail.com>
2021-10-28 17:42:56 +0000

    operator error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	else if (ptr == NULL && new_size == 0)
+	else if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
