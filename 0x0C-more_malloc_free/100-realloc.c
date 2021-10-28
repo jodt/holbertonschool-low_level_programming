d64dcef84b9370f3cf659aa8af5f85c7849c7e4e
jodt <joel.dumortier@gmail.com>
2021-10-28 16:23:43 +0000

    Project 4 completed

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of ptr
+ * @new_size: new size of ptr
+ *
+ * Return: pointer to a reallocated  memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, numberofelements;
+	char *new_ptr;
+	char *old_ptr = ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL && new_size == 0)
+		free(ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		new_ptr = malloc(new_size);
+		numberofelements = old_size / sizeof(char);
+		for (i = 0; i < numberofelements; i++)
+			new_ptr[i] = old_ptr[i];
+		free(ptr);
+	}
+	return (new_ptr);
+}
+
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -6,3 +6,4 @@
 * Project 1 : function that concatenates two strings
 * Project 2 : function that allocates memory for an array, using malloc
 * Project 3 : function that creates an array of integers
+* Project 4 : function that reallocates a memory block using malloc and free
