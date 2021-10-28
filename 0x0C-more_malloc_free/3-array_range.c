adc760b748059add3596973e08d00b5cfaeb8ef9
jodt <joel.dumortier@gmail.com>
2021-10-28 13:08:43 +0000

    project 3 completed

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * array_range - creates an array of integers
+ * @min: minimu value;
+ * @max: maximum value;
+ *
+ * Return: array of integers
+ */
+int *array_range(int min, int max)
+{
+	int *ptr, i, numberofelements;
+
+	numberofelements = max - min + 1;
+	if (min > max)
+		return (NULL);
+	if (min == max)
+	{
+		ptr = malloc(1 * sizeof(int));
+		if (ptr == NULL)
+			return (NULL);
+		*ptr = min;
+	}
+	if (min < max)
+	{
+		ptr = malloc(numberofelements * sizeof(int));
+		if (ptr == NULL)
+			return (NULL);
+		for (i = 0; i < numberofelements; i++)
+		{
+		       ptr[i] = min;
+		       min++;
+		}
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -5,3 +5,4 @@
 * Project 0 : function that allocates memory using malloc
 * Project 1 : function that concatenates two strings
 * Project 2 : function that allocates memory for an array, using malloc
+* Project 3 : function that creates an array of integers
