6039f9219b26da111e849041b456a03c714566ad
jodt <joel.dumortier@gmail.com>
2021-10-31 08:59:16 +0000

    bug fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef __FUNCTION_LIKE_MACRO_H__
 #define __FUNCTION_LIKE_MACRO_H__
-#define ABS(a) ((a) < 0 ? (-a) : (a))
+#define ABS(a) ((a) <= 0 ? (-a) : (a))
 #endif /*__FUNCTION_LIKE_MACRO_H__*/
