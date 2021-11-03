19430408e7a69b302c92d3f3361b8bf9e297d5b0
jodt <joel.dumortier@gmail.com>
2021-11-03 17:33:41 +0000

    add \n on printf

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(1);
 	}
 	if (atoi(argv[1]) < 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(2);
 	}
 	for (i = 0; i < atoi(argv[1]); i++)
