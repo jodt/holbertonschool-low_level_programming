a5827e6e699db7faf866a309bc4b934d53fa833c
jodt <joel.dumortier@gmail.com>
2021-11-03 20:07:25 +0000

    bug fix adresse pointer

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int (*ptr_main)(int, char **) = main;
 	int i;
-	char *ptr = (char *)&ptr_main;
+	char *ptr = (char *)ptr_main;
 
 	if (argc != 2)
 	{
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 0; i < atoi(argv[1]); i++)
 	{
-		printf("%02x", (unsigned char)*(ptr + i));
+		printf("%02x", (unsigned char) *(ptr + i));
 		if (i == (atoi(argv[1]) - 1))
 			printf("\n");
 		else
