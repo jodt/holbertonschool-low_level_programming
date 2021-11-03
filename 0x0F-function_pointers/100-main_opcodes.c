79c9bd24036cf0630cf3ed25ad027df60580c48a
jodt <joel.dumortier@gmail.com>
2021-11-03 19:06:05 +0000

    bug fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -25,15 +25,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
-	if (ptr_main != NULL)
-	{
 	for (i = 0; i < atoi(argv[1]); i++)
 	{
 		printf("%02x", (unsigned char)*(ptr + i));
 		printf(" ");
 	}
 	printf("\n");
-	}
 	return (0);
 }
 
