a5e9dd47f6710a684014f60f9430a1e26eb5f71c
jodt <joel.dumortier@gmail.com>
2021-11-03 17:32:41 +0000

    Project 4 completed

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+	int (*ptr_main)(int, char **) = main;
+	int i;
+	char *ptr = (char *)ptr_main;
+
+	if (argc != 2)
+	{
+		printf("Error");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]); i++)
+		printf("%02x\n", *(ptr + i));
+
+	return (0);
+}
+
