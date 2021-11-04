0e12c54587a3dc34b100d632247f9625543a9388
jodt <joel.dumortier@gmail.com>
2021-11-04 15:41:06 +0000

    update .h file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,4 +17,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_int(va_list arglist);
+void print_float(va_list arglist);
+void print_char(va_list arglist);
+void print_string(va_list arglist);
 #endif
