37cd33a2a540f148125fa37b9e94d549fc3b6447
jodt <joel.dumortier@gmail.com>
2021-11-04 13:23:22 +0000

    bug fix \n character

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,8 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(arglist, n);
-	if (n == 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arglist, int));
