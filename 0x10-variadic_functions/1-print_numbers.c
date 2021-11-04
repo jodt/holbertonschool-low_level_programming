ee64a1d57892ef2b2027fdc2aca18d7ba587f013
jodt <joel.dumortier@gmail.com>
2021-11-04 12:18:41 +0000

    bug fix separator NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arglist;
 	unsigned int i;
 
-	if (n == 0 && separator == NULL)
+	if (n == 0 || separator == NULL)
 		return;
 	va_start(arglist, n);
 	for (i = 0; i < n; i++)
