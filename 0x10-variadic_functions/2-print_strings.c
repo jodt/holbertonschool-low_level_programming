af70f6fed40c3cee8ac12ea073773c0fb33b12af
jodt <joel.dumortier@gmail.com>
2021-11-04 13:25:26 +0000

    bug fix \n character

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,4 @@
 #include"variadic_functions.h"
-#include<stdarg.h>
-#include<stdio.h>
-#include<stdlib.h>
 /**
  * print_strings - prints string
  * @separator: string printed between the strings
@@ -13,18 +10,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *string;
 
-	if (n == 0)
-		return;
 	va_start(arglist, n);
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(arglist, char*);
 		printf("%s", string == NULL ? "(nil)" : string);
-		if (i == n - 1)
-			printf("\n");
-		else
-			if (separator != NULL)
+			if (separator != NULL && i < n - 1)
 				printf("%s", separator);
 	}
+	printf("\n");
 	va_end(arglist);
 }
