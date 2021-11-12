4d92e6858c471d5e26096cc567fdf24dfc8dc00f
jodt <joel.dumortier@gmail.com>
2021-11-12 14:42:53 +0000

    refactoring code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,35 +8,24 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp, *new;
 	int i = 0;
+	list_t *temp, *new;
 
+	for (temp = *head; temp->next != NULL; temp = temp->next)
+		;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
 	while (str[i])
 		i++;
 	new->str = strdup(str);
 	if (new->str == NULL)
 	{
-		free(temp);
 		free(new);
 		return (NULL);
 	}
 	new->len = i;
 	new->next = NULL;
-	if (*head == NULL)
-		*head = new;
-	else
-		for (temp = *head; temp->next != NULL; temp = temp->next)
-			;
 	temp->next = new;
 	return (new);
 }
