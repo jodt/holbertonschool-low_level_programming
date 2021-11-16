33cc89c89204dd5578fecae6768517a05e5454de
jodt <joel.dumortier@gmail.com>
2021-11-16 08:34:39 +0000

    bug fix if index is not accessible

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,15 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
+	unsigned int number_of_elements = 0;
 
-	temp = head;
+	for (temp = head; temp->next != NULL; temp = temp->next)
+		number_of_elements++;
+	if ((index + 1) > number_of_elements)
+		return (NULL);
 	if (head == NULL)
 		return (NULL);
+
 	while (index > 0)
 	{
 		temp = temp->next;
