afd94564da6246adce7bbfec830a80ca11a510f7
jodt <joel.dumortier@gmail.com>
2021-11-16 08:17:26 +0000

    bug fix delete all nodes

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,7 +9,10 @@ void free_listint(listint_t *head)
 
 	if (head == NULL)
 		return;
-	temp = head;
-	head = head->next;
-	free(temp);
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
