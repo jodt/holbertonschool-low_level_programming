a403fbd522ee701834e7bd4fc86fabae00fb0fc6
jodt <joel.dumortier@gmail.com>
2021-11-16 09:02:55 +0000

    if list is empty,add one element

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *temp, *nodeafter;
 	unsigned int number_of_elements = 0;
 
-	if (head == NULL || *head == NULL ||  n == 0)
+	if (head == NULL || n == 0)
 		return (NULL);
 	for (temp = *head; temp->next != NULL; temp = temp->next)
 		number_of_elements++;
@@ -21,15 +21,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	temp = *head;
-	while (idx > 1)
-	{
-		temp = temp->next;
-		idx--;
-	}
 	new->n = n;
-	nodeafter = temp->next;
-	temp->next = new;
-	new->next = nodeafter;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		temp = *head;
+		while (idx > 1)
+		{
+			temp = temp->next;
+			idx--;
+		}
+		nodeafter = temp->next;
+		temp->next = new;
+		new->next = nodeafter;
+	}
 	return (new);
 }
