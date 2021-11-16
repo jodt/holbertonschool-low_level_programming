24f6d68cbd49955423f83291d89d4b551123b883
jodt <joel.dumortier@gmail.com>
2021-11-16 08:12:28 +0000

    project 4 completed

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include"lists.h"
+/**
+ * free_listint - free linked list
+ * @head : address of pointer to the first element*
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = head;
+	head = head->next;
+	free(temp);
+}
