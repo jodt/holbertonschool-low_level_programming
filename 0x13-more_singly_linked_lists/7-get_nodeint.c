f13ccdf6b29eecc3b07b66636a5e3696493939c4
jodt <joel.dumortier@gmail.com>
2021-11-16 08:39:20 +0000

    bug fix check if head is null

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int number_of_elements = 0;
 
+	if (head == NULL)
+		return (NULL);
 	for (temp = head; temp->next != NULL; temp = temp->next)
 		number_of_elements++;
 	if ((index + 1) > number_of_elements)
 		return (NULL);
 	temp = head;
-	if (head == NULL)
-		return (NULL);
-
 	while (index > 0)
 	{
 		temp = temp->next;
