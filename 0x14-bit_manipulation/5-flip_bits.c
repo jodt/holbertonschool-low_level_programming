b700061c8ff706927a3ee253038910caa4123efe
jodt <joel.dumortier@gmail.com>
2021-11-18 17:50:59 +0000

    bug fix condition if

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,9 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 
-	if (n == '\0' || m == '\0')
-		return (0);
-	for (i = 0; i < 32; i++)
+	for (i = 0; n != 0 || m != 0;  i++)
 	{
 		if (((n >> i) & 1) != ((m >> i) & 1))
 			count++;
