9eba44d9683bd4c6726028db45bb8663d0e7f146
jodt <joel.dumortier@gmail.com>
2021-11-18 18:10:38 +0000

    refactoring code and bug fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,15 +8,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0, difference;
+	unsigned int count = 0;
 
-	difference = n ^ m;
-
-	while (difference > 0)
+	while (n != 0 || m != 0)
 	{
-		if (difference & 1)
+		if ((n & 1) != (m & 1))
+		{
 			count++;
-		difference >>= 1;
+		}
+			n >>= 1;
+			m >>= 1;
 	}
 	return (count);
 }
