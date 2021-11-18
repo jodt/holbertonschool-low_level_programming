bf9761c1a39d7c1e84edd25e5f176f239e1b02a8
jodt <joel.dumortier@gmail.com>
2021-11-18 16:48:51 +0000

    project 5 completed

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * flip_bits - search number of bits to flip to get from one to antoher num.
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits modified
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+
+	for (i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+			count++;
+	}
+	return (count);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -4,6 +4,7 @@
 * Project 2 : function that returns the value of a bit at a given index.
 * Project 3 : function that sets the value of a bit to 1 at a given index.
 * Project 4 : function that sets the value of a bit to 0 at a given index.
+* Project 5 : function that returns the number of bits you would need to flip to get from one number to another.
 
 
 
