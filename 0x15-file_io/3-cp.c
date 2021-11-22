62903980189e6300d280002cb9c577cc90ab7e27
jodt <joel.dumortier@gmail.com>
2021-11-22 16:54:42 +0000

    bug fix error code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,26 +20,26 @@ int main(int argc, char **argv)
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		exit(99);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(2, "Error: Can't read to %s\n", argv[2]);
-		exit(99);
+		dprintf(2, "Error: Can't read from %s\n", argv[2]);
+		exit(98);
 	}
 	while (ch_read > 0)
 	{
 		ch_read = read(file_from, buf, BUFSIZE);
 		if (ch_read == -1)
 		{
-			dprintf(2, "Error: Can't read to %s\n", argv[2]);
-			exit(99);
+			dprintf(2, "Error: Can't read from %s\n", argv[2]);
+			exit(98);
 		}
 		if (write(file_to, buf, ch_read) == -1)
 		{
-			dprintf(2, "Error: Can't read to %s\n", argv[2]);
-			exit(98);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (close(file_to) == -1)
