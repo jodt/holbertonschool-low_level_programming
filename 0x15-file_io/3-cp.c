9c8ce0fb0f25620ba7cf83b29716f349a8fd956c
jodt <joel.dumortier@gmail.com>
2021-11-22 17:03:53 +0000

    rename fd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,19 +13,19 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(2, "Error: Can't read from %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[2]);
 		exit(98);
 	}
 	while (ch_read > 0)
@@ -33,23 +33,23 @@ int main(int argc, char **argv)
 		ch_read = read(file_from, buf, BUFSIZE);
 		if (ch_read == -1)
 		{
-			dprintf(2, "Error: Can't read from %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[2]);
 			exit(98);
 		}
 		if (write(file_to, buf, ch_read) == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (close(file_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	if (close(file_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
