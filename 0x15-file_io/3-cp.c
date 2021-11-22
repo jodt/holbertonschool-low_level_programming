6880b58282653bdf074a71a64c4068ddf5f31b70
jodt <joel.dumortier@gmail.com>
2021-11-22 16:08:18 +0000

    test code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char **argv)
 {
-	int file_to, file_from, ch_read;
+	int file_to, file_from, ch_read = BUFSIZE;
 	char buf[BUFSIZE];
 
 	if (argc != 3)
@@ -19,24 +19,27 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		exit(99);
+
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error : can't read to %s\n", argv[1]);
 		exit(99);
 	}
-	ch_read = read(file_from, buf, BUFSIZE);
-	if (ch_read == -1)
+	while (ch_read > 0)
 	{
-		dprintf(2, "Error : can't read to %s\n", argv[1]);
-		exit(99);
+		ch_read = read(file_from, buf, BUFSIZE);
+		if (ch_read == -1)
+		{
+			dprintf(2, "Error : can't read to %s\n", argv[1]);
+			exit(99);
+		}
+		if (write(file_to, buf, ch_read) == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(100);
+		}
 	}
-	if (write(file_to, buf, ch_read) == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(100);
-	}
-
 	if (close(file_to) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", file_to);
 	if (close(file_from) == -1)
