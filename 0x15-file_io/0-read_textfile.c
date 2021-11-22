b60648d896b287be2d97fbe5de6fd34c3abff46c
jodt <joel.dumortier@gmail.com>
2021-11-22 12:19:25 +0000

    remove '\0' character

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,15 +14,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	buff = malloc(sizeof(char) * (letters + 1));
-	if (buff == NULL)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buff = malloc(sizeof(char) * (letters));
+	if (buff == NULL)
+		return (0);
 	characters_number = read(fd, buff, letters);
-	buff[letters + 1] = '\0';
-
 	if (write(STDOUT_FILENO, buff, letters) == -1)
 		return (0);
 	free(buff);
