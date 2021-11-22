46746d4ed1ccff0c95cfdc92e85f9f401ab65a61
jodt <joel.dumortier@gmail.com>
2021-11-22 16:14:18 +0000

    bug fix code error exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,13 +18,16 @@ int main(int argc, char **argv)
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error : can't read to %s\n", argv[1]);
-		exit(99);
+		exit(98);
 	}
 	while (ch_read > 0)
 	{
@@ -32,17 +35,24 @@ int main(int argc, char **argv)
 		if (ch_read == -1)
 		{
 			dprintf(2, "Error : can't read to %s\n", argv[1]);
-			exit(99);
+			exit(98);
 		}
 		if (write(file_to, buf, ch_read) == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(100);
+			exit(99);
 		}
 	}
 	if (close(file_to) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	if (close(file_from) == -1)
+	{
 		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
 	return (0);
 }
