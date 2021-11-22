39b6ee1b28d0a63b39aa7689e1312fe15f07ba9a
jodt <joel.dumortier@gmail.com>
2021-11-22 11:42:58 +0000

    bug fix if fd = -1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == 1)
+	if (fd == -1)
 		return (0);
 
 	characters_number = read(fd, buff, letters);
