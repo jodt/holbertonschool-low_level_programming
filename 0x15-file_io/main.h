d2ec5d4fd88501176c3f1ecaa9a43c275e475645
jodt <joel.dumortier@gmail.com>
2021-11-22 15:50:34 +0000

    main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,11 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
+#define BUFSIZE 1024
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include<unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
