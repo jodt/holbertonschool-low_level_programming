0e3171c23ebe4c4411cd3669ab7a8ebd732205f5
jodt <joel.dumortier@gmail.com>
2021-11-22 12:09:54 +0000

    bug fix free

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	if (filename == NULL)
+	{
+		free(buff);
 		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
