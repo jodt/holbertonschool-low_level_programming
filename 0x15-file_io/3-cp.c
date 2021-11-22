c3a70d4bba529a2b92afa517daabb5a3508741c1
jodt <joel.dumortier@gmail.com>
2021-11-22 16:29:44 +0000

    refactoring code with error function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include"main.h"
+void error(char *mode, char *name_file, int error_code);
 /**
  * main - copies the content of a file to another file.
  * @argc: number of arguments
@@ -18,30 +19,17 @@ int main(int argc, char **argv)
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
+		error("write", argv[2], 99);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(2, "Error : can't read to %s\n", argv[1]);
-		exit(98);
-	}
+		error("read", argv[1], 98);
 	while (ch_read > 0)
 	{
 		ch_read = read(file_from, buf, BUFSIZE);
 		if (ch_read == -1)
-		{
-			dprintf(2, "Error : can't read to %s\n", argv[1]);
-			exit(98);
-		}
+			error("read", argv[1], 98);
 		if (write(file_to, buf, ch_read) == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			error("write", argv[2], 99);
 	}
 	if (close(file_to) == -1)
 	{
@@ -53,6 +41,18 @@ int main(int argc, char **argv)
 		dprintf(2, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	return (0);
+}
+
+
+/**
+ * error - print the error and exit
+ * @mode : read, write or close
+ * @name_file: name of the file ou fd
+ * @error_code: int of the error code
+ */
+void error(char *mode, char *name_file, int error_code)
+{
+	dprintf(2, "Error : can't %s to %s\n", mode, name_file);
+	exit(error_code);
 }
