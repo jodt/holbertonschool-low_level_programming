afba71575a665e952104c90fa9e6786f0914075c
jodt <joel.dumortier@gmail.com>
2022-01-06 11:31:27 +0000

    betty bug fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,7 @@
 #include "lists.h"
-
 /**
  * sum_dlistint - returns the sum of all the data (n) of a linked list
  * @head: pointer of the head element
-
  * Return: sum of all data
  */
 int sum_dlistint(dlistint_t *head)
