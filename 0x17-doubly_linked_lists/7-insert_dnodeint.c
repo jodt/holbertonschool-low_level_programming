9307b324f871f89c6a333a4e4c7e893414134b17
jodt <joel.dumortier@gmail.com>
2022-01-06 13:17:25 +0000

    betty bug fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
+ssize_t length_of_the_list(dlistint_t **h);
 /**
- * sum_dlistint - returns the sum of all the data (n) of a linked list
+ * insert_dnodeint_at_index - returns the sum of all the data of a linked list
  * @h: address of the pointer of the head element
- * @index: index where the new node will be insert
+ * @idx: index where the new node will be insert
  * @n: value of the new node
  *
  * Return: address of the new node
@@ -14,12 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	temp = *h;
-	while (temp)
-	{
-		temp = temp->next;
-		length++;
-	}
+	length = length_of_the_list(&*h);
 	if (idx > length)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
@@ -58,4 +54,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->next->prev = new;
 	temp->next = new;
 	return (new);
+}
+
+/**
+ * length_of_the_list - returns the length of a linked list
+ * @h: address of the pointer of the head element
+ *
+ * Return: length of the list
+ */
+ssize_t length_of_the_list(dlistint_t **h)
+{
+	dlistint_t *temp;
+	ssize_t length = 0;
+
+	temp = *h;
+	while (temp)
+	{
+		temp = temp->next;
+		length++;
+	}
+	return (length);
 }
