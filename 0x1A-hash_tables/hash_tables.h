c03d5d3e4abcfa235a175185c272d382e92e07cd
jodt <joel.dumortier@gmail.com>
2022-01-29 10:55:53 +0000

    bug fix alphabetic order

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -82,4 +82,5 @@ void shash_table_print(const shash_table_t *ht);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_delete(shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
+int compare_key(char *str, char *str2);
 #endif
