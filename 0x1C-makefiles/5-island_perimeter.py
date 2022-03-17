5f1f38716037f5cd785630cccc7420d264ec5d07
jodt <joel.dumortier@gmail.com>
2022-03-17 16:27:33 +0000

    test something

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,16 +10,9 @@ def island_perimeter(grid):
 
     Parameters:
     grid (list) : list of list of integers
-                0 represents a water zone
-                1 represents a land zone
-                One cell is a square with side length 1
-                Grid cells are connected horizontally/vertically.
-                Grid is rectangular, width and height don’t exceed 100
-                Grid is completely surrounded by water,
-                and there is one island (or nothing).
     """
     perimeter = 0
-    for i in range(1, len(grid)):
+    for i in range(1, len(grid)-1):
         for j in range(1, len(grid)):
             if grid[i][j] == 1:
                 if (grid[i-1][j]) == 0:
