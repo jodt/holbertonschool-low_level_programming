aee642926b4681855f8da0c52105c6efa8acc7c8
jodt <joel.dumortier@gmail.com>
2022-03-17 15:42:31 +0000

    add task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    if (len(grid) > 100):
+        return
+    for l in grid:
+        if len(l) > 100:
+            return
+    perimeter = 0
+    for i in range(1, len(grid)):
+        for j in range(1, len(grid)):
+            if grid[i][j] == 1:
+                if (grid[i-1][j]) == 0:
+                    perimeter += 1
+                if (grid[i][j-1]) == 0:
+                    perimeter += 1
+                if (grid[i][j+1]) == 0:
+                    perimeter += 1
+                if (grid[i+1][j]) == 0:
+                    perimeter += 1
+    return perimeter
