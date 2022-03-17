9398f806fb2283d20a70adce1287203c7ee9ea7b
jodt <joel.dumortier@gmail.com>
2022-03-17 16:29:41 +0000

    test something

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,7 +13,7 @@ def island_perimeter(grid):
     """
     perimeter = 0
     for i in range(1, len(grid)-1):
-        for j in range(1, len(grid)):
+        for j in range(1, len(grid[0])-1):
             if grid[i][j] == 1:
                 if (grid[i-1][j]) == 0:
                     perimeter += 1
