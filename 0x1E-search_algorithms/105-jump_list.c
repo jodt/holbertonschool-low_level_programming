ab84ae009a81b452b6a2dc3301bf86a5f360f36c
jodt <joel.dumortier@gmail.com>
2022-05-04 15:10:36 +0000

    add task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,10 +1,20 @@
 #include "search_algos.h"
 #include <math.h>
 
+/**
+ * jump_list - searches for a value in a sorted
+ * list of integers using the Jump search algorithm.
+ *
+ * @list: pointer to the first node
+ * @size: size of the list
+ * @value: value to find
+ * Return: node where value is located  else -1
+ */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	listint_t *start, *temp;
 	size_t jump, i = 0;
+
 	if (list)
 	{
 		jump = sqrt(size);
@@ -18,12 +28,13 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 			printf("Value checked at index [%ld] = [%d]\n", temp->index, temp->n);
 			jump += sqrt(size);
 		}
-		printf("Value found between indexes [%ld] and [%ld]\n", start->index, temp->index);
+		printf("Value found between indexes [%ld] and [%ld]\n",
+		 start->index, temp->index);
 		for (i = start->index; i <= temp->index; i++)
 		{
 			printf("Value checked at index [%ld] = [%d]\n", start->index, start->n);
 			if (value == start->n)
-				return start;
+				return (start);
 			start = start->next;
 		}
 	}
