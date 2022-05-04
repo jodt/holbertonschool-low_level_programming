9f8fab0e08bee30d82cd400f1313d7800e97435e
jodt <joel.dumortier@gmail.com>
2022-05-04 12:13:10 +0000

    add task 11

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+int binary_recur(int *array, int left, int right, size_t size, int value);
+
+/**
+ * advanced_binary - binary search function
+ *
+ * @array: array of integers
+ * @size: array size
+ * @value: value to find
+ * Return: value index or -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array)
+		return (binary_recur(array, 0, size - 1, size, value));
+	return (-1);
+}
+
+/**
+ * binary_recur - binary search using recursion
+ *
+ * @array: array of integers
+ * @left: start of the array
+ * @right: end of the array
+ * @size: array size
+ * @value: value to find
+ * Return: value index or -1
+ */
+int binary_recur(int *array, int left, int right, size_t size, int value)
+{
+	int middle = (left + right) / 2, i = 0;
+
+	if (left == right)
+	{
+		if (value == left)
+			return (left);
+		else
+			return (-1);
+	}
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		if (i == right)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+	if (value == middle)
+		return (middle);
+	else if (value < middle)
+		right = middle - 1;
+	else
+		left = middle + 1;
+	return (binary_recur(array, left, right, size, value));
+}
