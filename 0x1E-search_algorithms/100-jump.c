7e67894b93c9f24160638fe5886ab4cad487109b
jodt <joel.dumortier@gmail.com>
2022-05-04 07:38:53 +0000

    refactoring code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,22 +12,24 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t interval = sqrt(size);
-	size_t start = 0, jump = 0, i;
+	size_t jump = 0, start = 0, i;
 
 	if (array)
 	{
-		while (value > array[jump] && jump < size)
+		while (value > array[jump] && start < size)
 		{
+			if (jump > size)
+				break;
 			printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-				start = jump;
-				jump += interval;
+			start = jump;
+			jump += interval;
 		}
 		printf("Value found between indexes [%ld] and [%ld]\n", start, jump);
-		for (i = start; i < size ; i++)
+		for (i = start; i < size; i++)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
+			if (value == array[i])
+				return(i);
 		}
 	}
 	return (-1);
