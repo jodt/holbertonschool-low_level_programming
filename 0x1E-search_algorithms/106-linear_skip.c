683dfd44e01664f026ad78cc5c8ac7a3441cee9e
jodt <joel.dumortier@gmail.com>
2022-05-04 17:16:28 +0000

    add task 13

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *temp, *start, *end;
+
+	if (list)
+	{
+		start = end = list;
+		temp = list->express;
+
+		while (end->next)
+			end = end->next;
+		if (value < temp->n)
+			end = temp;
+		while (temp->express && value > temp->n)
+		{
+			printf("Value checked at index [%ld] = [%d]\n", temp->index, temp->n);
+			if (value < temp->express->n)
+			{
+				start = temp;
+				end = temp->express;
+				break;
+			}
+			temp = temp->express;
+			start = temp;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", end->index, end->n);
+		printf("Value found between indexes [%ld] and [%ld]\n", start->index, end->index);
+		while (start != end->next)
+		{
+			printf("Value checked at index [%ld] = [%d]\n", start->index, start->n);
+			if (value == start->n)
+				return start;
+			start = start->next;
+		}
+	}
+	return (NULL);
+}
